265dc7ab96907ae129164ab992a2b1a16cf4d272
pala <pala@MacBook-Pro.local>
2021-05-14 17:08:20 +0300

    add users app, move project to src folder, set up urls

--- src/conf/settings.py
+++ src/conf/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['0.0.0.0:8000']
 
 
 # Application definition
