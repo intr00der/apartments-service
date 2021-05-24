581b882bf262713c16980a201fc5de43cc9e5e93
Yaroslav Palaguta <palaguta.y.a@gmail.com>
2021-05-24 20:04:41 +0300

    delegate search logic to services.py

--- src/apps/apartments/services.py
+++ src/apps/apartments/services.py
@@ -1,5 +1,7 @@
 from django.contrib.auth import get_user_model
+from django.contrib.postgres.search import SearchVector
 
+from .forms import ApartmentFilteringForm
 from .models import Apartment, Country, City
 
 User = get_user_model()
@@ -36,5 +38,35 @@ def verify_apartment(apartment_pk):
     apartment.is_verified = True
     apartment.save()
 
-def search_by_query(request):
-    pass
+def filter_apartments_by_query(request):
+    apartments = Apartment.objects.filter(is_verified=True)
+    form = ApartmentFilteringForm(request.POST)
+
+    text_query = request.POST.get('search_bar')
+    daily_rate_query = request.POST.get('daily_rate')
+    square_area_query = request.POST.get('square_area')
+    room_amount_query = request.POST.get('room_amount')
+    bedroom_amount_query = request.POST.get('bedroom_amount')
+    convenience_item_query = request.POST.getlist('convenience_items')
+
+    if not square_area_query:
+        square_area_query = 0
+    if not room_amount_query:
+        room_amount_query = 0
+    if form.is_valid():
+        form.save(commit=False)
+
+        if text_query:
+            apartments = apartments.annotate(
+                search=SearchVector('country__name') + SearchVector('city__name') + SearchVector('description')
+            ).filter(search__icontains=text_query)
+        if square_area_query:
+            apartments = apartments.filter(square_area__gte=int(square_area_query))
+        if daily_rate_query:
+            apartments = apartments.filter(daily_rate__lte=daily_rate_query)
+        if room_amount_query != 0:
+            apartments = apartments.filter(room_amount__gte=room_amount_query)
+            apartments = apartments.filter(bedroom_amount__gte=bedroom_amount_query)
+        if convenience_item_query:
+            apartments = apartments.filter(convenience_items__contains=convenience_item_query)
+        return apartments, form
--- src/apps/apartments/views.py
+++ src/apps/apartments/views.py
@@ -1,52 +1,20 @@
 from django.contrib import messages
-from django.contrib.postgres.search import SearchVector
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
-from .forms import ApartmentForm, ApartmentFilteringForm
+from .forms import ApartmentForm
 from .models import Apartment
-from .services import create_apartment_by_form, verify_apartment
+from .services import (
+    create_apartment_by_form,
+    verify_apartment,
+    filter_apartments_by_query,
+)
 
 
 @login_required
 def home(request):
-    apartments = Apartment.objects.filter(is_verified=True)
-    form = ApartmentFilteringForm(request.POST)
-
-    text_query = request.POST.get('search_bar')
-    daily_rate_query = request.POST.get('daily_rate')
-    square_area_query = request.POST.get('square_area')
-    room_amount_query = request.POST.get('room_amount')
-    bedroom_amount_query = request.POST.get('bedroom_amount')
-    convenience_item_query = request.POST.getlist('convenience_items')
-
-    if not square_area_query:
-        square_area_query = 0
-    if not room_amount_query:
-        room_amount_query = 0
-    if form.is_valid():
-        form.save(commit=False)
-
-        # if request.GET.get:
-        # apartments = search_by_query(request)
-
-        if text_query:
-            apartments = apartments.annotate(
-                search=SearchVector('country__name') + SearchVector('city__name') + SearchVector('description')
-            ).filter(search__icontains=text_query)
-        if square_area_query:
-            apartments = apartments.filter(square_area__gte=int(square_area_query))
-        if daily_rate_query:
-            apartments = apartments.filter(daily_rate__lte=daily_rate_query)
-        if room_amount_query != 0:
-            apartments = apartments.filter(room_amount__gte=room_amount_query)
-            apartments = apartments.filter(bedroom_amount__gte=bedroom_amount_query)
-        if convenience_item_query:
-            apartments = apartments.filter(convenience_items__contains=convenience_item_query)
-        return render(request, 'apartments/home.html', {'apartments': apartments, 'form': form})
-    print(form.errors)
-
+    apartments, form = filter_apartments_by_query(request)
     return render(request, 'apartments/home.html', {'apartments': apartments, 'form': form})
 
 
