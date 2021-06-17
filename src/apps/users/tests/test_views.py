9113153179ba6f779b89a448e99386a3772bdacf
Yaroslav Palaguta <palaguta.y.a@gmail.com>
2021-06-18 00:30:57 +0300

    Add methods test_register_put_request, test_login to users.tests.test_views.UserManagerTestCase

--- src/apps/users/tests/test_views.py
+++ src/apps/users/tests/test_views.py
@@ -3,9 +3,10 @@ from django.test import RequestFactory, TestCase
 from apartments.models import Country, City
 from users.managers import UserManager
 from users.models import User
-from users.views import register
+from users.views import register, auth_login
 
 import tempfile
+import json
 
 
 class UserManagerTestCase(TestCase, UserManager):
@@ -26,17 +27,34 @@ class UserManagerTestCase(TestCase, UserManager):
             city_id=1,
             passport=tempfile.NamedTemporaryFile(suffix=".jpg").name
         )
+        cls.data = {'email': 'example@example.com',
+                    'password': 'example123',
+                    'confirm_password': 'example123',
+                    'first_name': 'John',
+                    'last_name': 'Doe',
+                    'gender': 1,
+                    'birthday': '1979-1-1',
+                    'country': 1,
+                    'city': 1}
 
     def test_register(self):
-        data = {'email': 'example@example.com',
-                'password': 'example123',
-                'confirm_password': 'example123',
-                'first_name': 'John',
-                'last_name': 'Doe',
-                'gender': 1,
-                'birthday': '1979-1-1',
-                'country': 1,
-                'city': 1}
-        request = self.factory.post('register/', data)
+        request = self.factory.post('register/', self.data)
         response = register(request)
+        self.assertEqual(response.status_code, 200)
+
+    def test_register_put_request(self):
+        new_data = self.data
+        new_data['email'] = 'testing@example.com'
+        request = self.factory.put('register/', self.data)
+        register(request)
+        self.assertEqual(
+            User.objects.filter(email=new_data['email']).exists(), False)
+
+    def test_login(self):
+        data = {
+            'email': self.login_user.email,
+            'password': self.login_user.password
+        }
+        request = self.factory.post('login/', data)
+        response = auth_login(request)
         self.assertEqual(response.status_code, 200)
